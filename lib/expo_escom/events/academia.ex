3deb9b6e66a0a28eb054c0ada3f885eed600c32e
Gerardo SC <gerardoserrc@gmail.com>
2024-12-12 20:34:29 -0600

    Agrega Academia y Docentes

---
+++ lib/expo_escom/events/academia.ex
@@ -0,0 +1,15 @@
+defmodule ExpoEscom.Eventos.Academia do
+  use Ash.Resource, domain: ExpoEscom.Eventos
+
+  attributes do
+    integer_primary_key :id
+
+    attribute :nombre, :string do
+      allow_nil? false
+    end
+  end
+
+  relationships do
+  end
+
+end
